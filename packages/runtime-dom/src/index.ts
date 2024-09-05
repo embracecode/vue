a2a3f0001ccd11383a5e760d59de29a3a597d6f1
jiangxianzhao <2459142408@qq.com>
2024-09-05 12:18:58 +0800

    对createRenderer的实现

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -5,11 +5,14 @@ import { nodeOps } from './nodeOps'
 
 import patchProp from './patchProps'
 
+import{ createRenderer } from '@vue/runtime-core'
 
 const renderOptions = Object.assign({ patchProp }, nodeOps)
 
-
-export {
-    renderOptions
+export const render = (vnode, container) => {
+    return createRenderer(renderOptions).render(vnode, container)
 }
-export * from '@vue/reactivity'
+
+export * from '@vue/runtime-core'
+
+// runtime-dom ---> runtime-core ---> reactivity
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -10,5 +10,5 @@ export function isRef(value) {
 export function isFunction(fn) {
   return typeof fn === 'function'
 }
-
+export * from './shapeFlags'
 
